6c51861edb12a144f92f53833b7fa147f0275764
plsletmeusethisid <85861478+plsletmeusethisid@users.noreply.github.com>
2026-04-10 14:35:41 +0900

    Add configuration settings to config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+VECTOR_DB_PATH    = "./vector_db"
+DATA_FILE_PATH    = "./company_data.txt"
+APP_ID       = "your_azure_app_id"
+APP_PASSWORD = "your_azure_app_password"
